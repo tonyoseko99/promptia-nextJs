1b2f9e6473d4725c325201474aff880a5f5325d8
Tonny Oseko <tonnyoseko0@gmail.com>
2023-06-16 21:30:15 +0300

    define a provider for login and logout

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+"use client";
+import { SessionProvider } from "next-auth/react";
 
-function Provider() {
-  return (
-    <div>Provider</div>
-  )
+function Provider({ children, session }) {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 }
 
-export default Provider
+export default Provider;
